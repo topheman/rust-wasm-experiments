35289ece20ca11a9d5c902f14d6c91faec3b4f22
Christophe Rosset <tophe@topheman.com>
2018-12-14 18:31:00 +0100

    feat(*): basic border collision + draw balls by JS

--- crate/src/ball.rs
+++ crate/src/ball.rs
@@ -9,16 +9,19 @@ pub struct Ball {
     pub y: f64,
     velocityX: f64,
     velocityY: f64,
+    pub radius: f64,
+    mass: f64,
     gravity: f64,
-    friction: f64,
+    elasticity: f64,
+    friction: f64
 }
 
 #[wasm_bindgen]
 impl Ball {
     #[wasm_bindgen(constructor, catch)]
-    pub fn new(x: f64, y: f64, velocityX: f64, velocityY: f64, gravity: f64, friction: f64) -> Ball {
+    pub fn new(x: f64, y: f64, velocityX: f64, velocityY: f64, radius: f64, mass: f64, gravity: f64, elasticity: f64, friction: f64) -> Ball {
         Ball {
-            x,y,velocityX,velocityY,gravity,friction
+            x, y, velocityX, velocityY, radius, mass, gravity, elasticity, friction
         }
     }
     pub fn step(&mut self) {
@@ -27,8 +30,31 @@ impl Ball {
         self.velocityX = self.friction * self.velocityX;
         self.velocityY = self.friction * self.velocityY;
     }
+    pub fn manageStageBorderCollision(&mut self, stageWidth: u32, stageHeight: u32) {
+        // left border
+        if self.x - self.radius < 0.0 {
+            self.velocityX = -self.velocityX * self.elasticity;
+            self.x = self.radius;
+        }
+        // right border
+        if self.x + self.radius > stageWidth as f64 {
+            self.velocityX = -self.velocityX * self.elasticity;
+            self.x = stageWidth as f64 - self.radius;
+        }
+        // top border
+        if self.y - self.radius < 0.0 {
+            self.velocityY = -self.velocityY * self.elasticity;
+            self.y = self.radius;
+        }
+        // bottom border
+        if self.y + self.radius > stageHeight as f64 {
+            self.velocityY = -self.velocityY * self.elasticity;
+            self.y = stageHeight as f64 - self.radius;
+        }
+    }
 }
 
+// Default trait doesn't seem to be currently supported by wasm-bingen
 impl Default for Ball {
     fn default() -> Self {
         Ball {
@@ -36,8 +62,11 @@ impl Default for Ball {
             y: 0.0,
             velocityX: 0.0,
             velocityY: 0.0,
+            radius: 10.0,
+            mass: 1.0,
             gravity: 1.0,
-            friction: 0.8,
+            elasticity: 0.98,
+            friction: 0.8
         }
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,46 +1,99 @@
-import("../crate/pkg").then(module => {
-  const balls = Array.from(Array(10), (_, index) => {
-    const position = index * 10;
-    const determinismValue = (index % 10) * 0.3; // 0 -> 1
-    return new module.Ball(
-      position,
-      position,
-      determinismValue,
-      determinismValue,
-      determinismValue,
-      determinismValue
-    );
-  });
+import { prepareCanvas } from "./canvas";
 
+import("../crate/pkg").then(module => {
+  /**
+   * Wrapper for wasm Ball constructor with defaults
+   */
+  function makeBall({
+    x = 0,
+    y = 0,
+    velocityX = 0,
+    velocityY = 0,
+    radius = 10,
+    mass = 1,
+    gravity = 1,
+    elasticity = 1,
+    friction = 1
+  } = {}) {
+    return new module.Ball(
+      x,
+      y,
+      velocityX,
+      velocityY,
+      radius,
+      mass,
+      gravity,
+      elasticity,
+      friction
+    );
+  }
+
+  /**
+   * Callback to pass to `prepareCanvas` that will be executed when window is resized
+   */
+  function onCanvasResize({ width, height }) {
+    console.log("resize", { width, height });
+    stageWidth = width;
+    stageHeight = height;
+  }
+
+  /**
+   * Prepares UI
+   */
   function prepare() {
     const rootNode = document.getElementById("root");
     const infosNode = document.createElement("ul");
     const cyclesNode = document.createElement("p");
-    const canvasRender = document.createElement("canvas");
-    canvasRender.className = "canvas-render";
-    const htmlRender = document.createElement("div");
-    htmlRender.className = "html-render";
+    const htmlRenderNode = document.createElement("div");
+    const { canvas: canvasRenderNode } = prepareCanvas({
+      onResize: onCanvasResize
+    });
+    htmlRenderNode.className = "html-render";
     rootNode.appendChild(infosNode);
     rootNode.appendChild(cyclesNode);
-    rootNode.appendChild(canvasRender);
-    rootNode.appendChild(htmlRender);
+    rootNode.appendChild(canvasRenderNode);
+    rootNode.appendChild(htmlRenderNode);
 
     return {
       rootNode,
       infosNode,
       cyclesNode,
-      canvasRender,
-      htmlRender
+      canvasRenderNode,
+      canvasCtx: canvasRenderNode.getContext("2d"),
+      htmlRenderNode
     };
   }
 
+  /**
+   * Update balls states
+   */
   function update(d) {
     delta = d;
     cycles++;
+    // move balls
     balls.forEach(ball => ball.step());
+    //check balls vs border collision
+    balls.forEach(ball =>
+      ball.manageStageBorderCollision(stageWidth, stageHeight)
+    );
   }
 
+  /**
+   * Helper to draw a ball directly with JavaScript code
+   */
+  function drawBallToCtx(ball, ctx) {
+    ctx.fillStyle = "#900000";
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, true);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  /**
+   * Draw balls
+   */
   function draw() {
+    // infos
     infosNode.innerHTML = balls
       .map(
         (ball, index) =>
@@ -52,8 +105,14 @@ import("../crate/pkg").then(module => {
     cyclesNode.innerText = `Cycles: ${cycles} - Delta: ${delta.toFixed(
       4
     )} - FrameRate: ${Math.round(1000 / delta)} FPS`;
+    // canvas
+    canvasCtx.clearRect(0, 0, stageWidth, stageHeight);
+    balls.forEach(ball => drawBallToCtx(ball, canvasCtx));
   }
 
+  /**
+   * Game loop
+   */
   function loop(timestamp) {
     if (cycles >= 4000) {
       return cancelAnimationFrame(loop);
@@ -64,11 +123,22 @@ import("../crate/pkg").then(module => {
     lastFrameTimeMs = timestamp;
   }
 
+  // Execution
+
   let delta = 0;
   let cycles = 0;
   let lastFrameTimeMs = 0;
 
-  const { rootNode, infosNode, cyclesNode, canvasRender } = prepare();
+  const balls = [
+    makeBall({ velocityX: 1, velocityY: 1 }),
+    makeBall({ velocityX: 4, velocityY: 1 }),
+    makeBall({ x: 300, y: 50, velocityX: -3, velocityY: 1 }),
+    makeBall({ x: 300, y: 200, velocityX: 7, velocityY: -5 })
+  ];
+
+  const { infosNode, cyclesNode, canvasCtx, canvasRenderNode } = prepare();
+  let stageWidth = canvasRenderNode.width;
+  let stageHeight = canvasRenderNode.height;
 
   loop();
 });
